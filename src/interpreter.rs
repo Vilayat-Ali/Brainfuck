3674b11a9fa7d2746f1e1fe54e7c792e8b8081d0
vilayat-ali <vilayatcodemysite@gmail.com>
2023-03-29 14:27:29 +0530

    Removed clap as dependency

---
+++ src/interpreter.rs
@@ -0,0 +1,73 @@
+#![allow(unused)]
+
+use crate::IO::{print_to_std_out, write_to_std_in};
+use std::{
+    fs::{read, File},
+    io::Read,
+    path::PathBuf,
+};
+
+const MAX_BUFFER_SIZE: usize = 50000;
+
+pub fn execute_program(file_path: &String) {
+    // memory array
+    let mut memory_block: [u8; 30000] = [0; 30000];
+    let mut pointer: usize = 0;
+    let mut loop_control: (bool, u8) = (false, 0); // loop control variables
+    match File::open(PathBuf::from(file_path)) {
+        Ok(mut source) => {
+            let mut buffer: [u8; MAX_BUFFER_SIZE] = [0; MAX_BUFFER_SIZE];
+            match source.read(&mut buffer) {
+                Ok(status) => {
+                    // parsing tokens
+                    for token in buffer.into_iter() {
+                        match token {
+                            // Plus Token
+                            43 => {
+                                memory_block[pointer] += 1;
+                            }
+                            // Minus Token
+                            45 => {
+                                memory_block[pointer] -= 1;
+                            }
+                            // Left Square Bracket Token
+                            91 => {
+                                // loop body starts
+                                loop_control.0 = true;
+                                loop_control.1 = memory_block[pointer];
+                            }
+                            // Right Square Bracket Token
+                            93 => {
+                                // loop body ends
+                                loop_control.0 = false;
+                                loop_control.1 = memory_block[pointer];
+                            }
+                            // Left Angle Bracket Token
+                            60 => {
+                                pointer -= 1;
+                            }
+                            // Right Angle Bracket Token
+                            62 => {
+                                pointer += 1;
+                            }
+                            // Period Token
+                            46 => {
+                                print_to_std_out(&[memory_block[pointer]]).unwrap();
+                            }
+                            // Comma Token
+                            44 => {
+                                if let Ok(input_val) = write_to_std_in() {
+                                    memory_block[pointer] = input_val;
+                                }
+                            }
+                            // Unrecognised characters are left as comments
+                            _ => {}
+                        }
+                    }
+                }
+                Err(e) => panic!("{}", e),
+            }
+        }
+        Err(e) => panic!("{}", e),
+    };
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
 #![allow(non_snake_case)]
 
 pub mod IO;
-pub mod tokens;
+pub mod interpreter;
